1998bf02ea2f791091ebdb84fad1fd70106e1445
onexev <onexevs@gmail.com>
2023-10-04 08:47:21 +0000

    Migration edit

--- services/ticket-service/migrations/env.py
+++ services/ticket-service/migrations/env.py
@@ -5,13 +5,13 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from app.database.db import SQLALCHEMY_DATABASE_URL
+from app.app import cfg
 from app.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', SQLALCHEMY_DATABASE_URL)
+config.set_main_option('sqlalchemy.url', cfg.pg_dsn.unicode_string())
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
